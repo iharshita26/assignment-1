874c3ddfb43e68a4d8a588e32c765b3b6067056c
Harshita Aggarwal <iharshita2614@gmail.com>
2026-01-31 22:12:22 +0530

    Add files via upload

---
+++ assignment 1 task 1.py
@@ -0,0 +1,11 @@
+num1 = int(input( "enter first number"))
+num2 = int(input( "enter second number"))
+add = num1 + num2
+print(add)
+sub = num1 - num2
+print(sub)
+mul = num1 * num2
+print(mul)
+div = num1 / num2
+print(div)
+           
